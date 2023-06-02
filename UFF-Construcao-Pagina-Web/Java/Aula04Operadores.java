0a2db2d058d8bcf4503e84f1809e266e36f0cd8b
Matheus Paizante <matheus_paizante@hotmail.com>
2023-06-01 23:02:25 -0300

    Update Aula04Operadores.java

--- UFF-Construcao-Pagina-Web/Java/Aula04Operadores.java
+++ UFF-Construcao-Pagina-Web/Java/Aula04Operadores.java
@@ -10,5 +10,23 @@ public class Aula04Operadores {
         System.out.println("Valor:" + (n3 - n1) + " outro valor:" + (n3+n1));
         System.out.println("Valor:" + (n3 / n1));
         System.out.println("Valor:" + n3 / n2);
+
+        int resto = 20 % 2;
+        int resto1 = 21 % 2;
+        System.out.println(resto);
+        System.out.println(resto1);//bom para verdadeiro ou falso
+
+        boolean isDezMaiorDoQueVinte = 10 > 20;
+        boolean isDezMenorDoQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDeQueVinte = 10 != 20;
+
+
+        System.out.println(isDezMaiorDoQueVinte);
+        System.out.println(isDezMenorDoQueVinte);
+        System.out.println(isDezIgualVinte);
+        System.out.println(isDezIgualDez);
+        System.out.println(isDezDiferenteDeQueVinte);
     }
 }
