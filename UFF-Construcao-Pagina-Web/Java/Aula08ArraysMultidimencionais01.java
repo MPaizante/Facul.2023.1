a26123460a72b17eeabeaa1ac36156217fc713dc
Matheus Paizante <matheus_paizante@hotmail.com>
2023-06-05 23:20:30 -0300

    Create Aula08ArraysMultidimencionais01.java

---
+++ UFF-Construcao-Pagina-Web/Java/Aula08ArraysMultidimencionais01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.Dojo.Java;
+
+public class Aula08ArraysMultidimencionais01 {
+    public static void main(String[] args) {
+
+        int [] [] dias = new int[3] [3];
+
+        for (int i = 0; i < dias.length; i++){
+            for (int j = 0; j < dias[0].length; j++){
+                System.out.println(dias[i][j]);
+            }
+
+        }
+
+
+
+    }
+}
