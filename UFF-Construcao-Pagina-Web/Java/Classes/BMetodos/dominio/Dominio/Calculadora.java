f20928d4c73beffec8e57b7b0333d1696902ca41
Matheus Paizante <matheus_paizante@hotmail.com>
2023-06-06 23:29:55 -0300

    .

---
+++ UFF-Construcao-Pagina-Web/Java/Classes/BMetodos/dominio/Dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package academy.devdojo.Dojo.Java.Classes.BMetodos.dominio.Dominio;
+
+public class Calculadora {
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+
+
+    public void subtraiDoisNumeros() {
+        System.out.println(30 - 4);
+    }
+}
