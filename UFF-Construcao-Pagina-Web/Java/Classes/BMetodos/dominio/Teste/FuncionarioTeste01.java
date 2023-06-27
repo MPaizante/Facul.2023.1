3b651527fa10cfe85df27146dabf225e0de5e86c
Matheus Paizante <matheus_paizante@hotmail.com>
2023-06-27 16:33:02 -0300

    .

--- UFF-Construcao-Pagina-Web/Java/Classes/BMetodos/dominio/Teste/FuncionarioTeste01.java
+++ UFF-Construcao-Pagina-Web/Java/Classes/BMetodos/dominio/Teste/FuncionarioTeste01.java
@@ -5,9 +5,9 @@ import academy.devdojo.Dojo.Java.Classes.BMetodos.dominio.Dominio.Funcionario;
 public class FuncionarioTeste01 {
     public static void main(String... args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Matheus";
-        funcionario.idade = 24;
-        funcionario.salarios = new double[]{3000, 3200, 3250};
+        funcionario.setNome("Matheus");
+        funcionario.setIdade(24);
+        funcionario.setSalarios(new double[]{3000, 3200, 3250});
 
         funcionario.imprime();
         funcionario.imprimeMediaSalario();
