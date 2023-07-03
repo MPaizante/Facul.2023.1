bef18b51e312c70b3b0d9766c57930cf58478f3e
Matheus Paizante <matheus_paizante@hotmail.com>
2023-07-03 20:28:35 -0300

    Create LeituraTecladoTest01.java

---
+++ UFF-Construcao-Pagina-Web/Java/2023/Gassociação/Teste/LeituraTecladoTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.Dojo.Java.Classes.Gassociação.Teste;
+
+import java.util.Scanner;
+
+public class LeituraTecladoTest01 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite seu nome:");
+        String nome = entrada.nextLine();
+        System.out.println("Digite sua idade:");
+        int idade = entrada.nextInt();
+        System.out.println("Digite seu sexo: (M/F)");
+        char sexo = entrada.next().charAt(0);
+        System.out.println("-----------------------------");
+        System.out.println("Nome: "+ nome);
+        System.out.println("Idade: "+ idade);
+        System.out.println("Sexo:  "+ sexo);
+
+
+    }
+}
