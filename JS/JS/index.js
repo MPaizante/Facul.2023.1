fa57c012ebe854d8e2abfb4e341f43db7f69f3a0
Matheus Paizante <matheus_paizante@hotmail.com>
2023-07-13 14:46:58 -0300

    Update index.js

--- JS/JS/index.js
+++ JS/JS/index.js
@@ -1,3 +1,13 @@
 document.getElementById("texto").innerHTML ="<i>Meu primeiro texto <b>JS</b></i>";
-alert("Oi isso é um aleta 5 + 4 = "+(5+4))
-console.log("Consle log olha ai meu querido.")
+alert("Oi isso é um aleta 5 + 4 = "+(5+4));
+console.log("Consle log olha ai meu querido.");
+
+let a, b , c;
+a = 5;
+b = 6;
+c = 7;
+console.log(a + b + c);
+
+function minhaFuncao(){
+
+};
