edbfc6870f2fb7e7e0b514bb341436d96f3cd561
Matheus Paizante <matheus_paizante@hotmail.com>
2023-07-17 20:51:33 -0300

    .

--- UFF-Construcao-Pagina-Web/Java/2023/iSobreEscrita/Dominio/Anime.java
+++ UFF-Construcao-Pagina-Web/Java/2023/iSobreEscrita/Dominio/Anime.java
@@ -1,4 +1,23 @@
 package academy.devdojo.Dojo.Java.Classes.iSobreEscrita.Dominio;
 
 public class Anime {
+    private String nome;
+
+    @Override
+    public String toString(){
+        return "Anime: " + this.nome;
+
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
---
+++ UFF-Construcao-Pagina-Web/Java/2023/iSobreEscrita/Teste/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.Dojo.Java.Classes.iSobreEscrita.Teste;
+
+import academy.devdojo.Dojo.Java.Classes.iSobreEscrita.Dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Naruto");
+        System.out.println(anime);
+    }
+}
