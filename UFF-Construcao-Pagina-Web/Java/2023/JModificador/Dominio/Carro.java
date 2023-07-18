61b5247c9f2a800a28079691df749fbb7dd5eb7f
Matheus Paizante <matheus_paizante@hotmail.com>
2023-07-17 21:10:46 -0300

    .

---
+++ UFF-Construcao-Pagina-Web/Java/2023/JModificador/Dominio/Carro.java
@@ -0,0 +1,14 @@
+package academy.devdojo.Dojo.Java.Classes.JModificador.Dominio;
+
+public class Carro {
+    public static final double velocidadeLimite = 250;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ UFF-Construcao-Pagina-Web/Java/2023/JModificador/Teste/CarroTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.Dojo.Java.Classes.JModificador.Teste;
+
+import academy.devdojo.Dojo.Java.Classes.JModificador.Dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.velocidadeLimite);
+
+    }
+}
