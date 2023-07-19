58cca4dc68b89b28fef4a8362a9b699d217d6c71
Matheus Paizante <matheus_paizante@hotmail.com>
2023-07-18 21:49:57 -0300

    Update Aula08.js

--- JS/JS/Aula08.js
+++ JS/JS/Aula08.js
@@ -1,4 +1,21 @@
 let n1 = 13;
 let n2 =  14;
 let res = n1 ^ n2;
-console.log(res)
+console.log(res);
+
+let a1 = [10 ,20, 30];
+let a2 = [11, 22 , 33];
+let a3 = [...a1] ;
+console.log(a1);
+console.log(a2);
+console.log(a3);
+
+const soma = (v1,v2,v3) => {
+    return v1 + v2 + v3;
+};
+console.log(soma(1,5,4))
+console.log(soma)
+
+let valores = [1,5,10];
+console.log(soma(...valores))
+
